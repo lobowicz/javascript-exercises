11dac97b2947470f37374f4a191c7e7e85192df6
lobowicz <josephakwasilobowicz@gmail.com>
2024-03-07 00:18:41 -0500

    update code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = "";
+    if (string === "") return ""; // check to see if string input is empty
+    if (num < 0) return "ERROR"; // if number is negative, ERROR
+    for (let i = 0; i < num; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
