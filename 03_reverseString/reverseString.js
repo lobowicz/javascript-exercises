97659abb2f695dbccb88bd757bb35775c92ab5e4
lobowicz <josephakwasilobowicz@gmail.com>
2024-03-08 02:26:01 -0500

    updated tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // parameter check
+    if (string === "") return "";
+    
+    output = "";
+    // for loop from the front but reverses the addition so its reversed
+    for (let i = 0; i < string.length; i++) {
+        output = string[i] + output;
+    }
+    return output;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+// getindexat
+// append it to string at start
